a5844403c47b20d2d0b50e724f921ffdb6d3ad01
Priyanshu <lcb2021003@iiitl.ac.in>
2023-12-23 01:28:43 +0530

    basic structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,13 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode:'class',
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors:{
+        "dark":'#1d1d1d',
+        "medium":"#2d2d2d"
+      }
     },
   },
   plugins: [],
